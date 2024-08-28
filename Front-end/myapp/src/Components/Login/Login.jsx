d955013a50ff75ab772352c11261a9bcd748105a
keerthivasan673 <keerthivasanramesh673@gmail.com>
2024-08-28 12:03:30 +0530

    Dropdown

--- Front-end/myapp/src/Components/Login/Login.jsx
+++ Front-end/myapp/src/Components/Login/Login.jsx
@@ -8,11 +8,25 @@ import password_icon from '../Assests/password.png'
 const Login =()=>{
 
     const [action,setAction] = useState("Login");
+    const [role,setRole] = useState("User");
+
+    const handleRoleChange = (e) => {
+        setRole(e.target.value);
+    }
     return (
         <div className = "container">
             <div className="header">
             <div className="text">{action}</div>
             <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={user_icon} alt="" />
+                    <select value={role} onChange={handleRoleChange}>
+                        <option value="User">User</option>
+                        <option value="Admin">Admin</option>
+                    </select>
+                </div>
             </div>
             <div className="inputs">
                 {action==="Login"?<div></div>:<div className="input">
